3e6755869120fbe1c44b7774171c6325e386507d
Jeffrey Wood <jeffreywood.dev@gmail.com>
2025-04-16 12:23:13 -0400

    bg

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
 	return (
-		<div className="font-playfair mt-8 sm:mt-20 min-h-screen sm:p-20">
+		<div className="font-playfair items-center flex flex-col mt-20 lg:mt-0 lg:justify-center min-h-screen sm:p-20">
 			<main className="flex flex-col gap-6 sm:gap-8 text-center">
-				<h1 className="font-playfair text-5xl font-semibold">
+				<h1 className="font-playfair text-6xl font-semibold">
 					DiceCTF x QuendCon
 				</h1>
 				<h2 className="text-4xl">- July 2025 -</h2>
-				<h2 className="text-4xl font-semibold">Brooklyn, NY</h2>
+				<h2 className="text-4xl underline font-semibold">Brooklyn, NY</h2>
 			</main>
 		</div>
 	);
