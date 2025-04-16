6ea60980e0a7271673a0d070ea8904691f1f52dc
Jeffrey Wood <jeffreywood.dev@gmail.com>
2025-04-16 11:49:53 -0400

    mt

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
 	return (
-		<div className="font-playfair grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+		<div className="font-playfair mt-20 grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<main className="flex flex-col gap-[32px] text-center row-start-2 items-center ">
-				<h1 className="font-playfair text-4xl font-semibold text-center sm:text-left">
+				<h1 className="font-playfair text-5xl font-semibold text-center sm:text-left">
 					DiceCTF x QuendCon
 				</h1>
-				<h2 className="text-4xl text-center">July 2025</h2>
-				<h2 className="text-4xl text-center">Brooklyn, NY</h2>
+				<h2 className="text-4xl text-center">- July 2025 -</h2>
+				<h2 className="text-4xl font-semibold text-center">Brooklyn, NY</h2>
 			</main>
 		</div>
 	);
