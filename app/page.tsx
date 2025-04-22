d58a668ea13f77f6eb9e99f6b2ecef63489d5b70
Jeffrey Wood <jeffreywood.dev@gmail.com>
2025-04-22 15:08:36 -0400

    new background

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 export default function Home() {
 	return (
-		<div className="font-playfair items-center flex flex-col mt-20 lg:mt-0 lg:justify-center min-h-screen sm:p-20">
-			<main className="flex flex-col gap-6 sm:gap-8 text-center">
-				<h1 className="font-playfair text-6xl font-semibold">
-					DiceCTF x QuendCon
+		<div className="font-playfair flex min-h-screen items-start mt-55 lg:mt-0 lg:items-center justify-center p-4 sm:p-20">
+			<main className="flex flex-col mr-5 gap-6 sm:gap-8 text-center">
+				<h1 className="font-playfair text-5xl font-semibold">
+					DiceCTF
+					<br />
+					<span className="text-3xl">x</span>
+					<br /> QuendCon
 				</h1>
-				<h2 className="text-4xl">- July 2025 -</h2>
-				<h2 className="text-4xl underline font-semibold">Brooklyn, NY</h2>
+				<h2 className="text-3xl">- July 2025 -</h2>
+				<h2 className="text-4xl border-b-2 border-white pb-1 font-semibold">
+					Brooklyn, NY
+				</h2>
 			</main>
 		</div>
 	);
